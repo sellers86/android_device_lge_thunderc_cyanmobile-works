273d63939e1333031e5f9cf39907094dda64d39b
BobZhome <BobZhome@Cheerful.com>
2011-11-16 18:45:00 -0500

    setup part 2

--- full_thunderc.mk
+++ full_thunderc.mk
@@ -35,6 +35,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Set those variables here to overwrite the inherited values.
 PRODUCT_NAME := full_thunderc
-PRODUCT_DEVICE := VM670
+PRODUCT_DEVICE := thunderc
 PRODUCT_BRAND := Android
 PRODUCT_MODEL := Full AOSP on Optimus V
