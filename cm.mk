4a78b5f20c9ab7616bafe0759ca1fb2018420ab6
BobZhome <BobZhome@Cheerful.com>
2011-11-27 10:53:15 -0500

    add cm.mk

---
+++ cm.mk
@@ -0,0 +1,13 @@
+$(call inherit-product, device/lge/thunderc/thunderc.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=thunderc BUILD_ID=GWK74 BUILD_DISPLAY_ID="GWK74 $(shell date +%m%d%Y)" BUILD_FINGERPRINT=lge/lge_gelato/VM701:2.3.4/GRJ22/ZV4.19cd75186d:user/release-keys PRIVATE_BUILD_DESC="lge_gelato-user 2.3.4 GRJ22 ZV4.19cd75186d release-keys
+
+PRODUCT_NAME := thunderc
+PRODUCT_DEVICE := VM670
+
