cb7bcc73af07ae1c23b78ec46821bb36cf0765e9
BobZhome <BobZhome@Cheerful.com>
2011-12-06 19:10:19 -0500

    maybe

--- cm.mk
+++ cm.mk
@@ -1,6 +1,6 @@
 $(call inherit-product, device/lge/thunderc/thunderc.mk)
 
-$(call inherit-product, device/lge/thunderc/device.mk)
+#$(call inherit-product, device/lge/thunderc/device.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
@@ -8,8 +8,18 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=thunderc BUILD_ID=GWK74 BUILD_DISPLAY_ID="GWK74 $(shell date +%m%d%Y)" BUILD_FINGERPRINT=lge/lge_gelato/VM701:2.3.4/GRJ22/ZV4.19cd75186d:user/release-keys PRIVATE_BUILD_DESC="lge_gelato-user 2.3.4 GRJ22 ZV4.19cd75186d release-keys
-
 PRODUCT_NAME := thunderc
-PRODUCT_DEVICE := VM670
+PRODUCT_BRAND := LGE
+PRODUCT_DEVICE := OPTIMUS
+PRODUCT_MODEL := VM670
+PRODUCT_MANUFACTURER := LGE
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+	BUILD_ID=GWK74 \
+	PRODUCT_NAME=${PRODUCT_MODEL} \
+	BUILD_DISPLAY_ID="GWK74 $(shell date +%m%d%Y)" \
+	BUILD_FINGERPRINT=lge/lge_gelato/VM701:2.3.4/GRJ22/ZV4.19cd75186d:user/release-keys \
+	PRIVATE_BUILD_DESC="lge_gelato-user 2.3.4 GRJ22 ZV4.19cd75186d release-keys" \
+	BUILD_NUMBER=${DATE}
+	TARGET_BUILD_TYPE=user
 
